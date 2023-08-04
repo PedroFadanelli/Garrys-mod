5137abe18cb7ab8f95962075e15ce171995848ea
Pedro Fadanelli <136035566+PedroFadanelli@users.noreply.github.com>
2023-08-03 22:25:24 -0300

    Update Player.gd
    Na hora do pulo nao tem a mesma velocidade que correndo

--- Labirinto mortal/Player.gd
+++ Labirinto mortal/Player.gd
@@ -1,40 +1,48 @@
 extends CharacterBody3D
 
-
 const SPEED = 9.0
+var CORRER = 20.0
 const JUMP_VELOCITY = 6.5
+const GRAVITY = 9.8
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.8
 
 @onready var camerabase = $CameraBase
-
-
+var is_running = false
 
 func _ready():
-	Input.mouse_mode = 2
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		camerabase.rotation.x -= deg_to_rad(event.relative.y * 1)
-		camerabase.rotation.x = clamp(camerabase.rotation.x, deg_to_rad(-90), deg_to_rad(90))
-		rotation.y -= deg_to_rad( event.relative.x * 1 )
-
+		camerabase.rotation.x = clamp(camerabase.rotation.x, deg_to_rad(15), deg_to_rad(45))
+		rotation.y -= deg_to_rad(event.relative.x * 1)
 
 func _physics_process(delta):
-	# Add the gravity.
+	# gravidade.
+	
+	if is_on_floor():
+		CORRER = 20
+	
 	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
+		velocity.y -= GRAVITY * delta
+		CORRER = 12
+	
+	# Pulando.
 	if Input.is_action_just_pressed("pula") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
+	# correndo
+	is_running = Input.is_action_pressed("correr") 
+		
+		
 	var input_dir = Input.get_vector("anda_esquerda", "anda_direita", "anda_frente", "anda_tras")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+
+	if is_running:
+		velocity.x = direction.x * CORRER
+		velocity.z = direction.z * CORRER
+	elif direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
